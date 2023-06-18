47915cbe3cf49a25937b1ecc63e0b6603f968015
Vibhanshu <vranjan198351@gmail.com>
2023-06-18 21:22:20 +0530

    adding server url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 
 // export const BASE_API_URL= "https://vblog-backend.onrender.com"
 // const BASE_API_URL= "https://vblog-backend.onrender.com";
-export const BASE_API_URL= "https://agritech-backend.vercel.app/"
+export const BASE_API_URL= "https://agritech-backend.vercel.app"
 // export const BASE_API_URL= "http://localhost:8000"
 
