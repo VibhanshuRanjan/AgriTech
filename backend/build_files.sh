c82879c8633346c1132764487d485b74adf92d61
Vibhanshu <vranjan198351@gmail.com>
2023-06-18 16:37:13 +0530

    correcting python version

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,5 +1,5 @@
 
 echo "BUILD START"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
 echo "BUILD END"
