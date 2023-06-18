7e9361ea69d90d69e2ba260fd68e205d94162148
Vibhanshu <vranjan198351@gmail.com>
2023-06-18 16:51:44 +0530

    updating build_files

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,5 +1,5 @@
 
 echo "BUILD START"
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+pip install -r requirements.txt
+python manage.py collectstatic 
 echo "BUILD END"
