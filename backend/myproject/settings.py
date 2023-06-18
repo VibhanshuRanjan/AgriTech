0e921349816cfe57e48b4cdee42b86bfa8531d7c
Vibhanshu <vranjan198351@gmail.com>
2023-06-18 19:05:54 +0530

    correcting build_files.sh

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 # STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 # MEDIA_URL= '/media/'
 # MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
@@ -150,8 +150,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # STATICFILES_DIRS = [
-#     # os.path.join(BASE_DIR, '../frontend/build/static'),
-#     os.path.join(BASE_DIR, '/static'),
+#     os.path.join(BASE_DIR, '../frontend/build/static'),
+#     # os.path.join(BASE_DIR, '/static'),
 # ]
 
 CORS_ORIGIN_ALLOW_ALL = True
